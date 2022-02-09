75bbab6a25bdcdca0eaa32919f78ff5730f50f7e
Pedro <krazyabyss@gmail.com>
2022-02-09 13:56:45 +0100

    Ejercicio Hecho

---
+++ js/Ejercicio2.js
@@ -0,0 +1,15 @@
+﻿window.onload = inicializaEventos;
+function inicializaEventos() {
+    document.getElementById("btnName").addEventListener("click", saludo, false);
+}
+function saludo() {
+    var miLlamada = new XMLHttpRequest();
+    miLlamada.open("GET", "https://crudpersonasaspcr7.azurewebsites.net/api/Persona");
+    miLlamada.onreadystatechange = function () {
+        if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            var arrayPersonas = JSON.parse(miLlamada.responseText);
+            document.getElementById("divNombre").innerHTML = arrayPersonas[0].apellidos;
+        }
+    }
+    miLlamada.send();
+}
